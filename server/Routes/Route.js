b4335dd007e5a6e68a4662d7e70abed53106da8e
“Matvey <matveytytarenko25@gmail.com>
2025-06-26 21:15:24 +0200

    Router

--- server/Routes/Route.js
+++ server/Routes/Route.js
@@ -2,6 +2,9 @@ const { GetInfo } = require('../Controller/Controller');
 
 const router = require('express').Router();
 
-router.post('/call', GetInfo);
+// router.post('/call', GetInfo);
+router.get('/call', (req, res) => {
+    res.status(200).json(req.body);
+})
 
 module.exports = router;
